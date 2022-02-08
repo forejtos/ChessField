675138f383dc492338f181b88e5053b092b9c850
forejt.filip.2018 <forejt.filip.2018@UC313PC07>
2022-02-08 13:32:36 +0100

    aaa

--- ChessField/Program.cs
+++ ChessField/Program.cs
@@ -22,6 +22,20 @@ namespace ChessField
             string vstupbila = "a2, b2, c3, f2, h4, Kb1, Vc1, Sd3, Se3";
             string vstupcerna = "a7, b7, c7, f7, g6, h5, Ke8, Va8, Sd6, Je5";
 
+            char[] separators = new char[] { ' ', ',' };
+            string[] subsb = vstupbila.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string[] subsc = vstupcerna.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var subb in subsb)
+            {
+                Console.WriteLine($"Substring bile: {subb}");
+            }
+            Console.WriteLine("");
+            foreach (var subc in subsc)
+            {
+                Console.WriteLine($"Substring cerne: {subc}");
+            }
+            Console.ReadKey();
         }
     }
 }
