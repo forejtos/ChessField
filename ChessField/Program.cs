9feaa40e7a4281f468fd1412f6686c9e5b8cfb94
forejt.filip.2018 <forejt.filip.2018@UC313PC07>
2022-02-08 13:14:25 +0100

    Přidejte soubory projektu.

---
+++ ChessField/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] sachovnice = new string[,] { {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "},
+                                                   {" ", " ", " ", " ", " ", " ", " ", " "} };
+
+            string vstupbila = "a2, b2, c3, f2, h4, Kb1, Vc1, Sd3, Se3";
+            string vstupcerna = "a7, b7, c7, f7, g6, h5, Ke8, Va8, Sd6, Je5";
+
+        }
+    }
+}
